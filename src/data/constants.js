ef2537d13f0e1fac816127ebd1d8c0b2e814d426
Ankush Sinha <97420654+ankushSinha1@users.noreply.github.com>
2024-02-15 22:21:26 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -8,7 +8,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/ankushSinha1",
   resume:
-    "https://drive.google.com/file/d/1Unt5nAkIRTl-OMTfzNFCzuGqheUgMdpj/view?usp=drive_link",
+    "https://drive.google.com/file/d/1_L32ONUnWByV2ypF-220z25DUJtVUeiA/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/ankush-sinha-355207223/",
   insta: "https://www.instagram.com/sinha_.ankush/",
 };
